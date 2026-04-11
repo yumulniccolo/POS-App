d588e5a7a9388bddce064fdbe93d58aaaf49a4e9
reinajessamine18 <joaquin_reinajessamine@plpasig.edu.ph>
2026-04-11 18:04:54 +0800

    Updated View Product Fragment with functioning buttons and toolbar

--- app/src/main/java/com/example/finalexer1grp2/viewProduct.java
+++ app/src/main/java/com/example/finalexer1grp2/viewProduct.java
@@ -9,13 +9,18 @@ import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class viewProduct extends Fragment {
 
+    private int quantity = 1;
     public viewProduct() {
-        // Required empty constructor
     }
 
     @Override
@@ -31,12 +36,19 @@ public class viewProduct extends Fragment {
         TextView price = view.findViewById(R.id.pPrice);
         TextView desc = view.findViewById(R.id.pDescription);
         ImageView image = view.findViewById(R.id.imageView);
+        MaterialToolbar toolbar = view.findViewById(R.id.fragment_cart_toolbar);
+
+        ImageButton minusBtn = view.findViewById(R.id.minusBtn);
+        ImageButton addBtn = view.findViewById(R.id.addBtn);
+        TextView tvQuantity = view.findViewById(R.id.tvQuantity);
+        Button btnAddToCart = view.findViewById(R.id.btnAddToCart);
 
         if (getArguments() != null) {
             name.setText(getArguments().getString("name"));
             price.setText(String.format("₱%.2f", getArguments().getDouble("price")));
             desc.setText(getArguments().getString("desc"));
             image.setImageResource(getArguments().getInt("image"));
+            toolbar.setTitle(getArguments().getString("name"));
 
             desc.setText(
                 Html.fromHtml(
@@ -45,5 +57,31 @@ public class viewProduct extends Fragment {
                     )
             );
         }
+
+        toolbar.setNavigationOnClickListener(v ->
+                requireActivity().getOnBackPressedDispatcher().onBackPressed()
+        );
+
+        addBtn.setOnClickListener(v -> {
+            quantity++;
+            tvQuantity.setText(String.valueOf(quantity));
+        });
+
+        minusBtn.setOnClickListener(v -> {
+            if (quantity > 1) {
+                quantity--;
+                tvQuantity.setText(String.valueOf(quantity));
+            }
+        });
+
+        btnAddToCart.setOnClickListener(v -> {
+            String productName = getArguments().getString("name");
+
+            Toast.makeText(requireContext(), quantity + " " + productName + " added to cart", Toast.LENGTH_SHORT).show();
+
+            requireActivity()
+                    .getOnBackPressedDispatcher()
+                    .onBackPressed();
+        });
     }
 }
