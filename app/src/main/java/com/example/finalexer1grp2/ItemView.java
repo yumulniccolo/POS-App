ad1061137911283c4724fd63be8bd24e7bffe020
Niccolo Yumul <yumul_niccolofranco@plpasig.edu.ph>
2026-04-12 12:09:47 +0800

    Unfinished bottom bar layout for subtotal

--- app/src/main/java/com/example/finalexer1grp2/ItemView.java
+++ app/src/main/java/com/example/finalexer1grp2/ItemView.java
@@ -18,16 +18,16 @@ import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
 
-public class viewProduct extends Fragment {
+public class ItemView extends Fragment {
 
     private int quantity = 1;
 
-    public viewProduct() {
+    public ItemView() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_view_product, container, false);
+        return inflater.inflate(R.layout.fragment_item_view, container, false);
     }
 
     @Override
